9b91f5bad9251ae86acde649bdd7d33c2e76753b
Robert <36878317+robbowz@users.noreply.github.com>
2018-09-21 09:47:59 +0200

    Update and rename onlytor2.sh to onlytor3.sh

--- onlytor3.sh
+++ onlytor3.sh
@@ -54,55 +54,57 @@ echo "Done..."
 echo ""
 
 
-	echo "Do you want to install several masternodes?"
-	echo "If so, enter the amount of masternodes you would like to install followed by [ENTER] Maximum 10: "
-   	read $number
-   	echo "We will now begin to install the pre-requisites and $number of XUEZ Coin masternodes."
-   	echo ""
-
 # TOR INSTALLATION
-#    sudo su -c "echo 'deb http://deb.torproject.org/torproject.org '$(lsb_release -c | cut -f2)' main' > /etc/apt/sources.list.d/torproject.list" 
-#	gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 
-#	gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add - 
-#	sudo apt-get update 
-#	sudo apt-get -y install tor deb.torproject.org-keyring 
-#	sudo usermod -a -G debian-tor $(whoami) 
-#
-#	sudo sed -i 's/#ControlPort 9051/ControlPort 9051/g' /etc/tor/torrc
-#	sudo sed -i 's/#CookieAuthentication 1/CookieAuthentication 1/g' /etc/tor/torrc
-#	sudo su -c "echo 'CookieAuthFileGroupReadable 1' >> /etc/tor/torrc"
-#	sudo su -c "echo 'LongLivedPorts 9033' >> /etc/tor/torrc"
-#	sudo su -c "echo 'HiddenServiceDir /etc/tor/hidden_service/"
-#	sudo systemctl restart tor.service
-#	sleep 10
-# 	TORHOSTNAME=`cat /etc/tor/hidden_service/hostname`
+        sudo su -c "echo 'deb http://deb.torproject.org/torproject.org '$(lsb_release -c | cut -f2)' main' > /etc/apt/sources.list.d/torproject.list" 
+	gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 
+	gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add - 
+	sudo apt-get update 
+	sudo apt-get -y install tor deb.torproject.org-keyring 
+	sudo usermod -a -G debian-tor $(whoami) 
 
-### Install TOR ###
-echo "Installing TOR..."
-apt-get -qq install tor
-cat >> /etc/tor/torrc << EOL
+	sudo sed -i 's/#ControlPort 9051/ControlPort 9051/g' /etc/tor/torrc
+	sudo sed -i 's/#CookieAuthentication 1/CookieAuthentication 1/g' /etc/tor/torrc
+	sudo su -c "echo 'CookieAuthFileGroupReadable 1' >> /etc/tor/torrc"
+	sudo su -c "echo 'LongLivedPorts 9033' >> /etc/tor/torrc"
+	sudo su -c "echo 'HiddenServiceDir /etc/tor/hidden_service/"
+	sudo systemctl restart tor.service
+	cat >> /etc/tor/torrc << EOL
+	EOL
+	/etc/init.d/tor stop
+	sudo rm -R /etc/tor/hidden_service 2>/dev/null
+	/etc/init.d/tor start
+	echo "Starting TOR, please wait..."
+	sleep 5 # Give tor enough time to connect before we continue
+	sleep 10
+ 	TORHOSTNAME=`cat /etc/tor/hidden_service/hostname`
+
+
+#### Install TOR ###
+#echo "Installing TOR..."
+#apt-get -qq install tor
+#cat >> /etc/tor/torrc << EOL
 ### XUEZ CONFIGURATION ###
-HiddenServiceDir /var/lib/tor/hidden_service/
-ClientOnly 1
-ControlPort 9051
-NumEntryGuards 4
-NumDirectoryGuards 3
-GuardLifetime 2764800
-GeoIPExcludeUnknown 1
-HiddenServiceDir /var/lib/tor/hidden_service/
-HiddenServicePort 9033 127.0.0.1:52543
-HiddenServicePort 80 127.0.0.1:80
-LongLivedPorts 80,9033
-EOL
-  /etc/init.d/tor stop
-  sudo rm -R /var/lib/tor/hidden_service 2>/dev/null
-  /etc/init.d/tor start
-  echo "Starting TOR, please wait..."
-  sleep 5 # Give tor enough time to connect before we continue
+#HiddenServiceDir /var/lib/tor/hidden_service/
+#ClientOnly 1
+#ControlPort 9051
+#NumEntryGuards 4
+#NumDirectoryGuards 3
+#GuardLifetime 2764800
+#GeoIPExcludeUnknown 1
+#HiddenServiceDir /var/lib/tor/hidden_service/
+#HiddenServicePort 9033 127.0.0.1:52543
+#HiddenServicePort 80 127.0.0.1:80
+#LongLivedPorts 80,9033
+#EOL
+#  /etc/init.d/tor stop
+#  sudo rm -R /var/lib/tor/hidden_service 2>/dev/null
+#  /etc/init.d/tor start
+#  echo "Starting TOR, please wait..."
+#  sleep 5 # Give tor enough time to connect before we continue
 
 
 # Set TORHOSTNAME
-  TORHOSTNAME=`cat /var/lib/tor/hidden_service/hostname`
+#  TORHOSTNAME=`cat /var/lib/tor/hidden_service/hostname`
 	
 
 #	cd ~
